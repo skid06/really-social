1f7d306e46335921073c3129f4e6249717644fa7
Melchor Valencia <valencia.mel.06@gmail.com>
2025-04-11 12:03:21 +0800

    feat: add migrations and model for blog

---
+++ backend/app/Models/Blog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Blog extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'status',
+        'created_by'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
